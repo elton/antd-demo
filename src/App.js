329403353a96005fc3a39590880ceb4bfed1cbea
Elton Zheng <elton.zheng.sh@gmail.com>
2020-08-06 22:00:38 +0800

    add Button component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DatePicker, message, Alert, Button } from "antd";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import "antd/dist/antd.css";
+import "./App.scss";
 
-function App() {
+moment.locale("zh-cn");
+
+const ButtonDemo = () => (
+  <>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </>
+);
+
+const DateDemo = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = (value) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format("YYYY年MM月DD日") : "未选择"}`
+    );
+    setDate(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 400, margin: "100px auto" }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        <Alert
+          message="当前日期"
+          description={date ? date.format("YYYY年MM月DD日") : "未选择"}
+        />
+      </div>
     </div>
   );
-}
+};
+
+const App = () => (
+  <>
+    <p>
+      <DateDemo />
+    </p>
+    <p>
+      <ButtonDemo />
+    </p>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ConfigProvider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
